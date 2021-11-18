0ade74c114a8a9dcd8c031a1935650c693cae40f
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2021-11-17 21:29:15 -0300

    setup springboot project

---
+++ doaae/src/main/java/br/com/fiap/doaae/DoaaeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.doaae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoaaeApplication.class, args);
+	}
+
+}
---
+++ doaae/src/test/java/br/com/fiap/doaae/DoaaeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.doaae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
